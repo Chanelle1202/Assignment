dadfc92ed5a85bbce7482853f3a7995d7fcc6f80
Chanelle Lee <chanelle1202@googlemail.com>
2016-04-18 10:04:40 +0100

    GA Results

--- Genfis22LinkGA.m
+++ Genfis22LinkGA.m
@@ -2,5 +2,5 @@
 
 lb = [0.1; 0.1; 0.1; 0.1; 0.1; 0.1]; %lower constraint as radii values must be positive
 ub = [0.9; 0.9; 0.9; 0.9; 0.9; 0.9]; %upper constraint as radii values must be <= 1
-opts = optimoptions(@ga, 'MaxTime', 60, 'PlotFcn',{@gaplotbestf,@gaplotmaxconstr},'Display','iter');
+opts = gaoptimset('Generations', 20, 'PopulationSize', 10, 'PlotFcn',{@gaplotbestf,@gaplotmaxconstr});
 [x, fval, exitflag, output, population] = ga(@ANFIS_IK_2Link_Genfis2, 6, [],[],[],[],lb,ub, [], opts);
